558e209f75e8d523803a784591099f816d37f4ee
Claire Limesand <clairelimesand@gmial.com>
2022-01-02 18:16:17 -0600

    created friendForm and added it to nav bar

---
+++ src/components/FriendForm/FriendForm.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import {useDispatch, useSelector} from 'react-redux';
+import useReduxStore from '../../hooks/useReduxStore';
+
+function FriendForm() {
+
+    return (
+    <div>
+        <p>Hola</p>
+    </div>
+    );
+}
+
+// this allows us to use <App /> in index.js
+export default FriendForm;
