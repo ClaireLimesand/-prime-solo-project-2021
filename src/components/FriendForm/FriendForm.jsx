0c902a2c1b63b6742bb83aaeb5c9221baa561dc2
Claire Limesand <clairelimesand@gmial.com>
2022-01-02 18:51:16 -0600

    added textfield for addfriend

--- src/components/FriendForm/FriendForm.jsx
+++ src/components/FriendForm/FriendForm.jsx
@@ -1,13 +1,56 @@
 import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useHistory } from "react-router";
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useDispatch, useSelector} from 'react-redux';
 import useReduxStore from '../../hooks/useReduxStore';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import swal from 'sweetalert2';
 
 function FriendForm() {
 
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const [friendName, setFriendName] = useState('');
+
+    const handleSaveButton = () => {
+        const newFriend = {
+            name: friendName
+        }
+        console.log('new friend', newFriend);
+        if (
+            friendName === ''
+        ) {
+            // swal({
+            //     title: "Hey wait,",
+            //     text: "your friend needs a name!",
+            // });
+        } else {
+            dispatch({
+                type: 'ADD_FRIEND',
+                payload: newFriend
+            })
+            history.push('/user');
+        };  
+    }
+
     return (
     <div>
-        <p>Hola</p>
+        <p>Add your friend here:</p>
+            <TextField 
+                type="text"
+                value={friendName}
+                onChange={(event) => setFriendName(event.target.value)}
+                required
+            />
+            <Button
+                id="saveButton"
+                onClick={handleSaveButton}
+            >
+                Add This Friend
+            </Button>
     </div>
     );
 }
